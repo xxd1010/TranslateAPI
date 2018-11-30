7da5f745fb2c0777b5e7d3a4b2f21e6c1a5cfde3
youkissaki <45446971+youkissaki@users.noreply.github.com>
2018-11-30 12:41:43 +0800

    Add files via upload

---
+++ Google翻译接口.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 22:23:55 2018
+
+@author: kissaki
+"""
+import random
+from urllib import parse
+import requests
+
+from lxml import etree
+
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/45.0.2454.85 Safari/537.36 115Browser/6.0.3',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+]
+
+
+def UrlCode(text, code):
+    # URL编码
+    if code == 1:
+        text = parse.quote(text)
+    # URl解码
+    elif code == 2:
+        text = parse.unquote(text)
+    return text
+
+
+def PostData(Encodetext):
+    # https://translate.google.cn/translate_a/single?
+    # client=t&sl=en&tl=zh-CN&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=1&tk=164573.263968&q=run
+    url = "https://translate.google.cn/translate_a/single?"
+    res = requests.get(url)
+    print(res)
+
+
+if __name__ == '__main__':
+    text = input('翻译内容: ')
+    # UrlCode函数对 text 编码,  1   表示编码代号
+    text = UrlCode(text, 1)
+    PostData(text)
